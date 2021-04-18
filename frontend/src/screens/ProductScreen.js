822d5c4cd52ba3c882337892a2ce66643a06ad57
Ida <so.kida@hotmail.com>
2021-04-18 15:59:39 -0400

    redux

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,51 +1,80 @@
-import React, { Component } from 'react';
-import './ProductScreen.css'
+import "./ProductScreen.css";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-export default class ProductScreen extends Component {
-    render() {
-        return (
-            <div className='productscreen'>
-                
-                <div className='productscreen__left'>
-                    <div className='left__image'>
-                        <img src='https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80' alt='product name'/>
-                    </div>
+// Actions
+import { getProductDetails } from "../redux/actions/productActions";
+import { addToCart } from "../redux/actions/cartActions";
 
-                    <div className='left__info'>
-                        <p className='left__name'>Product 1</p>
-                        <p >Price: $400</p>
-                        <p >kd jsakljdflksajfkdlsjf klsdj fdlsk jfdlsak fjkdls fjkl. kldjfla, lkdjkals.</p>
-                    </div>
-                
-                </div>
+const ProductScreen = ({ match, history }) => {
+  const [qty, setQty] = useState(1);
+  const dispatch = useDispatch();
 
-                <div className='productscreen__right'>
-                    <div className='right__info'>
-                    <p>
-                    Price: <span>$400</span>
-                    </p>
-                    <p>
-                        Status: <span>In stock</span>
-                    </p>
-                    <p>Qty
+  const productDetails = useSelector((state) => state.getProductDetails);
+  const { loading, error, product } = productDetails;
 
-                    <select>
-                        <option value='1'> 1</option>
-                        <option value='2'> 2</option>
-                        <option value='3'> 3</option>
-                        <option value='4'> 4</option>
-                    </select>
-                    </p>
-
-                    <p>
-                        <button type='button'>Add to cart</button>
-                    </p>
-
-                    </div>
-
-                </div>
-
-            </div>
-        )
+  useEffect(() => {
+    if (product && match.params.id !== product._id) {
+      dispatch(getProductDetails(match.params.id));
     }
-}
+  }, [dispatch, match, product]);
+
+  const addToCartHandler = () => {
+    dispatch(addToCart(product._id, qty));
+    history.push(`/cart`);
+  };
+
+  return (
+    <div className="productscreen">
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h2>{error}</h2>
+      ) : (
+        <>
+          <div className="productscreen__left">
+            <div className="left__image">
+              <img src={product.imageUrl} alt={product.name} />
+            </div>
+            <div className="left__info">
+              <p className="left__name">PlayStation 5</p>
+              <p>Price: ${product.price}</p>
+              <p>Description: {product.description}</p>
+            </div>
+          </div>
+          <div className="productscreen__right">
+            <div className="right__info">
+              <p>
+                Price:
+                <span>${product.price}</span>
+              </p>
+              <p>
+                Status:
+                <span>
+                  {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                </span>
+              </p>
+              <p>
+                Qty
+                <select value={qty} onChange={(e) => setQty(e.target.value)}>
+                  {[...Array(product.countInStock).keys()].map((x) => (
+                    <option key={x + 1} value={x + 1}>
+                      {x + 1}
+                    </option>
+                  ))}
+                </select>
+              </p>
+              <p>
+                <button type="button" onClick={addToCartHandler}>
+                  Add To Cart
+                </button>
+              </p>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default ProductScreen;
